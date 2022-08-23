512bcd2c6b3c4b24ec972e0c0e6157814a533e20
AkshayWazir <waizaeer4@gmail.com>
2022-08-23 17:10:12 +0530

    conditional rendering

--- src/components/custom/productItem/index.jsx
+++ src/components/custom/productItem/index.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import AddToCart from "../addToCart";
 import "./styles.css";
 
 export default function ProductItem(props) {
-  return <div></div>;
+  const { data = { name: "", img: "", detail: "" } } = props;
+  const [items, setItems] = useState(0);
+
+  return (
+    <div className="product-item-cont">
+      <img src={data.img} alt="product" className="product-item-image" />
+      <span style={{ padding: "1vh" }}>
+        <p className="product-item-title">{data.name}</p>
+        <p className="product-item-detail">{data.detail}</p>
+      </span>
+      <AddToCart
+        count={items}
+        addToCart={() => setItems(items + 1)}
+        removeFromCart={() => setItems(items - 1)}
+      />
+    </div>
+  );
 }
