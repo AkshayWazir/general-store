a62d55028a9a05a5c9debaebb035608b015d1f23
AkshayWazir <waizaeer4@gmail.com>
2022-08-25 16:53:41 +0530

    Akshay | 123 | added mobile screen

--- src/screens/browsing/mobile/index.jsx
+++ src/screens/browsing/mobile/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles.css";
+import { getBrowsingData } from "../../../api/browsingAPI";
+import { ProductItem } from "../../../components";
 
 export default function Mobile(props) {
-  return <div>Mobile</div>;
+  const data = getBrowsingData();
+  return (
+    <div className="browsing-content">
+      {data.map((item) => (
+        <ProductItem data={item} />
+      ))}
+    </div>
+  );
 }
